cc3d047e0c50531ce4f5284978e3fe222a86e65e
Kyra <kyra.mooren@mailbox.org>
2023-09-27 11:33:54 +0200

    add validation and error message, add kfzjson

--- search.js
+++ search.js
@@ -1,15 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
     var searchButton = document.querySelector('.btn-primary');
-    var kfzInput = document.querySelector('#kfz');
+    var searchTerm = document.querySelector('#searchTerm');
     var outputDiv = document.querySelector('#output');
+    var errorDiv = document.querySelector('#error');
+
+    outputDiv.appendChild(document.createElement('div'))
+    errorDiv.appendChild(document.createElement('div'))
 
     searchButton.addEventListener('click', function () {
-        var inputValue = kfzInput.value.trim().toUpperCase();
-        if (inputValue !== '') {
+        var inputValue = searchTerm.value.trim().toUpperCase();
+        if (isInputValid(inputValue)) {
             var newRow = document.createElement('div');
             newRow.textContent = inputValue;
-            outputDiv.appendChild(newRow);
-            kfzInput.value = '';
+            outputDiv.replaceChild(newRow, outputDiv.children[0]);
+        } else {
+            var newRow = document.createElement('div');
+            newRow.textContent = "Falsches Format, nur gültige KFZ-Kennzeichen werden akzeptiert";
+            errorDiv.replaceChild(newRow, errorDiv.children[0]);    
         }
+        searchTerm.value = '';
     });
 });
+
+function isInputValid(inputValue) {
+    if(inputValue != '' && isKFZPattern(inputValue)){
+        return true;
+    } else {
+        console.log("Input invalid")
+        return false
+    }
+}
+
+function isKFZPattern(inputValue){
+    var regexPattern = /[a-zA-ZÄÜÖäöü]{1,3}/;
+    var regex = new RegExp(regexPattern);
+    return regex.test(inputValue);
+}
+
